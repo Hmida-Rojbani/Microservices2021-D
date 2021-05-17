47151b2e1ef1da0a8104b912f08be1f9165694d7
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-05-17 09:52:06 +0100

    microservices

---
+++ UserService/src/main/java/de/tekup/service/controller/UserController.java
@@ -0,0 +1,15 @@
+package de.tekup.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@GetMapping
+	public String works() {
+		return "User services works";
+	}
+}
